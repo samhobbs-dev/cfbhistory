2c5b7be1381a2a72dfdde067bfd11264a1a9aef7
SamuelHobbsHCL <110624235+SamuelHobbsHCL@users.noreply.github.com>
2023-03-13 00:56:43 -0500

    Minor revisions

--- cfbh-frontend/src/App.tsx
+++ cfbh-frontend/src/App.tsx
@@ -4,16 +4,11 @@ import ConfGrid from './component/ConfGrid';
 import TeamSchedule from './component/TeamSchedule';
 
 function App() {
-  let teamId: number = 251;
-  let year: number = 1985;
+  let year: number = 2002;
   return (
     <div className="App">
       <header className="App-header">
         <ConfGrid year={year}/>
-        <TeamSchedule
-          teamId={teamId}
-          year={year}
-        />
       </header> 
     </div>
   );
