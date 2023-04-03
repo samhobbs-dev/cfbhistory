124dd7bf3c368b154f36bc08ce782e5894c8cb0b
SamuelHobbsHCL <110624235+SamuelHobbsHCL@users.noreply.github.com>
2023-04-03 16:13:03 -0500

    Removed /server in deploy scripts, changed nvm version

--- cfbh-frontend/scripts/before_install.sh
+++ cfbh-frontend/scripts/before_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ec2-user/server
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+cd /home/ec2-user
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 nvm install node
 nvm install-latest-npm
