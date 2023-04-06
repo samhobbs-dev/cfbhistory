f5890b421eba8794aa87ad9ae581fd43b486609e
SamuelHobbsHCL <110624235+SamuelHobbsHCL@users.noreply.github.com>
2023-04-06 14:12:50 -0500

    Replaced shebang w/source command

--- cfbh-frontend/scripts/after_install.sh
+++ cfbh-frontend/scripts/after_install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+source /home/ec2-user/.bash_profile
 cd /home/ec2-user/cfbh-frontend
 nvm -v
 npm install
--- cfbh-frontend/scripts/before_install.sh
+++ cfbh-frontend/scripts/before_install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+source /home/ec2-user/.bash_profile
 cd /home/ec2-user
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
