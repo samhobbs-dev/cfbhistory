e63d4ef553e939e6b4a8978b882ccf69377bb6ba
SamuelHobbsHCL <110624235+SamuelHobbsHCL@users.noreply.github.com>
2023-04-06 13:11:03 -0500

    Fixed scripts' nvm issues & destination

--- cfbh-frontend/scripts/before_install.sh
+++ cfbh-frontend/scripts/before_install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cd /home/ec2-user
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm -v
 nvm install node
 nvm install-latest-npm
