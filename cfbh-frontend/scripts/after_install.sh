472ce326d25ceccd457501e30c4c6c1390ab067b
SamuelHobbsHCL <110624235+SamuelHobbsHCL@users.noreply.github.com>
2023-04-10 15:32:16 -0500

    Added pm2 to shell scripts

--- cfbh-frontend/scripts/after_install.sh
+++ cfbh-frontend/scripts/after_install.sh
@@ -2,5 +2,4 @@ source /home/ec2-user/.bash_profile
 cd /home/ec2-user/cfbh-frontend
 nvm -v
 npm install
-# npm install --save react react-dom react-scripts react-particles-js
-# npm install pm2 -g
+npm install pm2 -g
--- cfbh-frontend/scripts/app_start.sh
+++ cfbh-frontend/scripts/app_start.sh
@@ -1,8 +1,3 @@
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/cfbh-frontend
-ls -ltR /home/ec2-user
-npm start
-# pm2 start npm --name "covidapp" -- start
-# pm2 startup
-# pm2 save
-# pm2 restart all
+pm2 serve --name "cfbh-app" build 3000 --spa
