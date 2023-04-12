efe18ab68e44952278ca7692d5fe93cd5f348366
SamuelHobbsHCL <110624235+SamuelHobbsHCL@users.noreply.github.com>
2023-04-12 15:18:24 -0500

    Added/renamed build files & scripts (for backend), & more

---
+++ cfbh-backend/scripts/app_start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/ec2-user
+java -jar cfbh-backend-1.0.jar > /dev/null 2> /dev/null < /dev/null &
--- cfbh-backend/src/test/java/com/cfbh/cfbhbackend/CfbhBackendApplicationTests.java
+++ cfbh-backend/src/test/java/com/cfbh/cfbhbackend/CfbhBackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.cfbh.cfbhbackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = CfbhBackendApplication.class)
 class CfbhBackendApplicationTests {
 
 	@Test
