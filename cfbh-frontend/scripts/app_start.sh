e36bf24f0492f2675342e438f6a563a9401df121
SamuelHobbsHCL <110624235+SamuelHobbsHCL@users.noreply.github.com>
2023-04-19 16:03:28 -0500

    BE yml now overwrites jars, FE start script if app not running

--- cfbh-frontend/scripts/app_start.sh
+++ cfbh-frontend/scripts/app_start.sh
@@ -1,3 +1,4 @@
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/cfbh-frontend
-pm2 serve --name "cfbh-app" build 3000 --spa
+# If no app running, start
+if ! netstat -ano | grep :3000; then pm2 serve --name 'cfbh-app' build 3000 --spa; fi
