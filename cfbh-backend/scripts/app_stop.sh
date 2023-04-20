86fe28ec4304b3b390ecdd53c52a9d6f2fed07c2
SamuelHobbsHCL <110624235+SamuelHobbsHCL@users.noreply.github.com>
2023-04-20 13:53:04 -0500

    Changed BE stop & start script

--- cfbh-backend/scripts/app_stop.sh
+++ cfbh-backend/scripts/app_stop.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-ps -ef | grep cfbh-backend-1.0.jar | grep -v grep | awk '{print $2}' | xargs kill
+# Kill any process already on port 8081, including jar if already running
+fuser -k 8081/tcp
+# ps -ef | grep cfbh-backend-1.0.jar | grep -v grep | awk '{print $2}' | xargs kill
