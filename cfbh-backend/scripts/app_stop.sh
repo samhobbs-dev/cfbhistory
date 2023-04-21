15b93774f963ba495c854cac0014843d5ab021f7
SamuelHobbsHCL <110624235+SamuelHobbsHCL@users.noreply.github.com>
2023-04-21 12:12:27 -0500

    Modified BE stop script to prevent returning error code 1

--- cfbh-backend/scripts/app_stop.sh
+++ cfbh-backend/scripts/app_stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Kill any process already on port 8081, including jar if already running
-fuser -k 8081/tcp
+if fuser 8081/tcp; then fuser -k 8081/tcp; fi
 # ps -ef | grep cfbh-backend-1.0.jar | grep -v grep | awk '{print $2}' | xargs kill
