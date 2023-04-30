eafee18cbf97562a7c0e359b110b9c791ab6d5cc
SamuelHobbsHCL <110624235+SamuelHobbsHCL@users.noreply.github.com>
2023-04-30 15:47:40 -0500

    Added different hosts for environments, tab icon & desc.

--- cfbh-frontend/src/api/recordService.tsx
+++ cfbh-frontend/src/api/recordService.tsx
@@ -37,7 +37,7 @@ type RecordTeamResponse = {
 }
 
 // TODO strict typing & async requests?
-export const HOST = 'http://ec2-18-189-66-35.us-east-2.compute.amazonaws.com:8081'
+export const HOST = process.env.REACT_APP_BACKEND_URL;
 const RecordService = {
     // API calls
     async getAllTeamRecordsByYear(year: number) {
