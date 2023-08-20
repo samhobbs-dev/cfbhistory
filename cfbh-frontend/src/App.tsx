4764213be185175de3ccf0e39aa7b86d5f9ccaa7
SamuelHobbsHCL <sam_hobbs1@alumni.baylor.edu>
2023-08-20 11:19:49 -0500

    Fixed additional eslint errors

--- cfbh-frontend/src/App.tsx
+++ cfbh-frontend/src/App.tsx
@@ -1,39 +1,39 @@
 import React from 'react';
 import './App.css';
 import SchedulePage from './page/SchedulePage';
-import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Navigate, Routes } from 'react-router-dom';
 import Header from './component/Header';
 import Footer from './component/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Header/>
-      </header>
-      <main className="App-body">
-        <BrowserRouter>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <Navigate to="year/2022"/>
-              }
-            />
-            <Route
-              path="year/:year"
-              element={
-                <SchedulePage/>
-              }
-            />
-          </Routes>
-        </BrowserRouter>
-      </main>
-      <footer className="App-footer">
-        <Footer/>
-      </footer>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Header/>
+            </header>
+            <main className="App-body">
+                <BrowserRouter>
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={
+                                <Navigate to="year/2022"/>
+                            }
+                        />
+                        <Route
+                            path="year/:year"
+                            element={
+                                <SchedulePage/>
+                            }
+                        />
+                    </Routes>
+                </BrowserRouter>
+            </main>
+            <footer className="App-footer">
+                <Footer/>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
