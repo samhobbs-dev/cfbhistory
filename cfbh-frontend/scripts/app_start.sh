0e7344914a0820c29de170cae36fe03f9d4be9cc
SamuelHobbsHCL <sam_hobbs1@alumni.baylor.edu>
2023-09-24 17:25:53 -0500

    Modified frontend to run on same machine as backend

--- cfbh-frontend/scripts/app_start.sh
+++ cfbh-frontend/scripts/app_start.sh
@@ -1,4 +1,4 @@
-source /home/ec2-user/.bash_profile
-cd /home/ec2-user/cfbh-frontend
+source /home/ubuntu/.bash_profile
+cd /home/ubuntu/cfbh-frontend
 # If no app running, start
 if ! netstat -ano | grep :3000; then pm2 serve --name 'cfbh-app' build 3000 --spa; fi
