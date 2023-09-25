537134aeffd4ad8094f9412a6ddc14c0bb270466
SamuelHobbsHCL <sam_hobbs1@alumni.baylor.edu>
2023-09-24 20:02:46 -0500

    Missing revisions

--- cfbh-frontend/scripts/after_install.sh
+++ cfbh-frontend/scripts/after_install.sh
@@ -1,8 +1,8 @@
-source /home/ec2-user/.bash_profile
-cd /home/ec2-user/cfbh-frontend
+source /home/ubuntu/.profile
+cd /home/ubuntu/cfbh-frontend
 nvm -v
 npm install
 npm install pm2 -g
-sudo yum install nginx -y
-sudo mv ningx-redirect.conf /etc/nginx/conf.d/ningx-redirect.conf
+sudo apt install nginx -y
+sudo mv nginx-redirect.conf /etc/nginx/conf.d/nginx-redirect.conf
 sudo service nginx start
