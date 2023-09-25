635a0fb4aaedb3ea087bbc127166b5721eeed491
SamuelHobbsHCL <sam_hobbs1@alumni.baylor.edu>
2023-09-24 21:54:00 -0500

    Replaced netstat w/ss in app_start.sh

--- cfbh-frontend/scripts/app_start.sh
+++ cfbh-frontend/scripts/app_start.sh
@@ -1,4 +1,4 @@
 source /home/ubuntu/.profile
 cd /home/ubuntu/cfbh-frontend
 # If no app running, start
-if ! netstat -ano | grep :3000; then pm2 serve --name 'cfbh-app' build 3000 --spa; fi
+if ! ss -ltn | grep :3000; then pm2 serve --name 'cfbh-app' build 3000 --spa; fi
