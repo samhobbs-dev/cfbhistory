44354136a1468ae98090ab8d275c5bf1f5bfca22
SamuelHobbsHCL <sam_hobbs1@alumni.baylor.edu>
2023-09-25 23:14:19 -0500

    Added check for pm2

--- cfbh-frontend/scripts/after_install.sh
+++ cfbh-frontend/scripts/after_install.sh
@@ -3,6 +3,7 @@ cd /home/ubuntu/cfbh-frontend
 nvm -v
 npm install
 npm install pm2 -g
+pm2 -v
 sudo apt install nginx -y
 sudo mv nginx-redirect.conf /etc/nginx/conf.d/nginx-redirect.conf
 sudo service nginx start
--- cfbh-frontend/scripts/app_start.sh
+++ cfbh-frontend/scripts/app_start.sh
@@ -1,4 +1,5 @@
 source /home/ubuntu/.profile
 cd /home/ubuntu/cfbh-frontend
+pm2 -v
 # If no app running, start
 if ! ss -ltn | grep :3000; then pm2 serve --name 'cfbh-app' build 3000 --spa; fi
