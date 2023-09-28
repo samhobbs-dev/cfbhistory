79687e1e6c80aa4a7cc52ba31a1afe4a1c6d4b74
SamuelHobbsHCL <sam_hobbs1@alumni.baylor.edu>
2023-09-28 17:44:39 -0500

    Added full path to pm2, plus -v test

--- cfbh-frontend/scripts/app_start.sh
+++ cfbh-frontend/scripts/app_start.sh
@@ -1,5 +1,7 @@
 source /home/ubuntu/.profile
 cd /home/ubuntu/cfbh-frontend
-pm2 -v
+nvm -v
+npm -v
+/home/ubuntu/.nvm/versions/node/v20.7.0/bin/pm2 -v
 # If no app running, start
 if ! ss -ltn | grep :3000; then pm2 serve --name 'cfbh-app' build 3000 --spa; fi
