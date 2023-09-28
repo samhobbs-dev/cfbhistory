6dcfe7d7a644bfb1b21b5a6d43d8637d4a9e43da
SamuelHobbsHCL <sam_hobbs1@alumni.baylor.edu>
2023-09-28 18:17:26 -0500

    Manually adding path to pm2 for now

--- cfbh-frontend/scripts/app_start.sh
+++ cfbh-frontend/scripts/app_start.sh
@@ -1,9 +1,8 @@
-echo $HOME
 source /home/ubuntu/.profile
-source /home/ubuntu/.bashrc
+# source /home/ubuntu/.bashrc
 cd /home/ubuntu/cfbh-frontend
-nvm -v
-npm -v
-/home/ubuntu/.nvm/versions/node/v20.7.0/bin/pm2 -v
+# nvm -v
+# npm -v
+# /home/ubuntu/.nvm/versions/node/v20.7.0/bin/pm2 -v
 # If no app running, start
-if ! ss -ltn | grep :3000; then pm2 serve --name 'cfbh-app' build 3000 --spa; fi
+if ! ss -ltn | grep :3000; then /home/ubuntu/.nvm/versions/node/v20.7.0/bin/pm2 serve --name 'cfbh-app' build 3000 --spa; fi
